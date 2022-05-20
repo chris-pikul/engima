0ad873c573b11106a22a7189513bcb6537cfc81c
Chris Pikul <chris@workmaple.com>
2022-05-20 09:50:08 -0700

    starting tests for Enigma

---
+++ tests/Enigma.test.ts
@@ -0,0 +1,23 @@
+// test src/Enigma.ts
+import { expect } from 'chai';
+import { getCharacterFromIndex, getCharacterIndex } from '../src/alphabet';
+import Enigma from '../src/Enigma';
+import * as Models from '../src/models';
+
+describe('Enigma class', () => {
+  describe('Model D functional test', () => {
+    const mod = new Enigma(Models.ModelD);
+    mod.installWheel('I');
+    mod.installWheel('II');
+    mod.installWheel('III');
+
+    it('encodes an entire message correctly', () => {
+      mod.reset();
+
+      const input = 'TestMessage';
+      const expected = 'ZCLG JFTA YON';
+
+      expect(mod.encodeMessage(input)).to.equal(expected);
+    });
+  });
+});
